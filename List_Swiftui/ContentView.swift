81fbe0d46cb6928e28963019bf7dd1fe1fefd9f1
ruiyaa <nini2525min@gmail.com>
2025-07-11 20:32:33 +0900

    Initial Commit

---
+++ List_Swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List_Swiftui
+//
+//  Created by tanaka niko on 2025/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List_Swiftui/List_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_SwiftuiApp.swift
+//  List_Swiftui
+//
+//  Created by tanaka niko on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct List_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
